29e85061b80f29911c461fbf962fe2ec09159ae8
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-11-15 22:21:23 -0800

    11/15/2017 my first commit

---
+++ Documents/workspace1/practice/src/com/csinc/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.csinc;
+
+/**
+ * Created by Bakam on 11/15/17.
+ */
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("OM");
+
+    }
+}

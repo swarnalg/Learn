b2ef061ce5468bc7c826643bad8f47ea0c2f9e02
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-11-18 00:23:33 -0800

    11/17/2017 SwapingNumbers program commit

---
+++ Documents/workspace1/practice/src/com/csinc/PracticePrograms.java
@@ -0,0 +1,14 @@
+package com.csinc;
+
+/**
+ * Created by Bakam on 11/18/17.
+ */
+public class PracticePrograms {
+    public static void main(String args[]){
+        SwapingNumbers sn = new SwapingNumbers();
+        sn.SwappNumbers();
+
+
+
+    }
+}
---
+++ Documents/workspace1/practice/src/com/csinc/SwapingNumbers.java
@@ -0,0 +1,26 @@
+package com.csinc;
+
+/**
+ * Created by Bakam on 11/17/17.
+ */
+public class SwapingNumbers {
+    public SwapingNumbers(){
+
+    }
+    public void SwappNumbers(){
+        int a=10;
+        int b=20;
+        int c;
+        System.out.println("a was"+a);
+        System.out.println("b was"+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("a is now"+a);
+        System.out.println("b is now"+b);
+
+
+
+    }
+
+}

06459e213bec9b58a5a4aa0e1241e460a4b774cc
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-11-21 17:53:43 -0800

    11/21/2017 minimal program commit

---
+++ Documents/workspace1/practice/src/com/csinc/Minimal.java
@@ -0,0 +1,17 @@
+package com.csinc;
+
+/**
+ * Created by Bakam on 11/21/17.
+ */
+public class Minimal {
+    public void Min(int c,int d){
+    int m;
+        if(c<d)
+            m=c;
+        else
+            m=d;
+        System.out.println("minimum number is"+m);
+
+
+    }
+}

122e5cd8b8cdbc84e5ad4ba082442b275d062e30
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-11-29 09:08:59 -0800

    11/29/2017 Sum of digits 1 1+2=3 1+2+3=6 .... ... ...

---
+++ Documents/workspace1/practice/src/com/csinc/SumOfDigits.java
@@ -0,0 +1,21 @@
+package com.csinc;
+
+/**
+ * Created by Bakam on 11/29/17.
+ * Write a program that displays the sum of numbers from 1 to 10 by lines:
+ */
+public class SumOfDigits {
+    public static void main(String[] args)
+    {
+        //add your code here
+        int s=0;
+        for(int i=1;i<=10;i++)
+        {
+            s=s+i;
+
+            System.out.println(s);
+        }
+
+    }
+
+}

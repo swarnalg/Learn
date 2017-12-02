2ec3a2550bcd80f598d45822402e2ceb9b150f58
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-12-01 18:21:39 -0800

    12/01/2017 Added  to multiplication Table

--- Documents/workspace1/practice/src/com/csinc/PracticePrograms.java
+++ Documents/workspace1/practice/src/com/csinc/PracticePrograms.java
@@ -7,8 +7,9 @@ public class PracticePrograms {
     public static void main(String args[]){
         SwapingNumbers sn = new SwapingNumbers();
         sn.SwappNumbers();
-
-
-
+        Minimal m1=new Minimal();
+        m1.min(11,12);
+       MultiplicationTable m=new MultiplicationTable();
+        m.Multi();
     }
 }

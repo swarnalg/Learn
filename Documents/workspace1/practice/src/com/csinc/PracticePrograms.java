d14e371bc300734cf878be2f01f254eeb3813b2f
swarnalg <32751976+swarnalg@users.noreply.github.com>
2017-12-06 17:14:16 -0800

    12/06/2017 Added Reading data from the keyboard  to  commit

--- Documents/workspace1/practice/src/com/csinc/PracticePrograms.java
+++ Documents/workspace1/practice/src/com/csinc/PracticePrograms.java
@@ -1,15 +1,20 @@
 package com.csinc;
 
+import java.io.IOException;
+
 /**
  * Created by Bakam on 11/18/17.
  */
 public class PracticePrograms {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
         SwapingNumbers sn = new SwapingNumbers();
+        MultiplicationTable m=new MultiplicationTable();
+        InputString i=new InputString();
         sn.SwappNumbers();
         Minimal m1=new Minimal();
         m1.min(11,12);
-       MultiplicationTable m=new MultiplicationTable();
+
         m.Multi();
+        i.inputData();
     }
 }

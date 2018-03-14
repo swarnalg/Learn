396058b5912ffee0a18ba097c8de7a805afadd79
swarnalg <32751976+swarnalg@users.noreply.github.com>
2018-03-13 20:03:34 -0700

    renaming class ArraylistTask6

--- Documents/workspace1/practice/src/com/csinc/EvenOddTask.java
+++ Documents/workspace1/practice/src/com/csinc/EvenOddTask.java
@@ -1,11 +1,15 @@
 package com.csinc;
 
-// Reading list of numbers from keyboard,even numbers are added to the end of the list,odd numbers are added to the beginning of the list
+// Reading list of numbers from keyboard,
+// even numbers are added to the end of the list,
+// odd numbers are added to the beginning of the list
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-public class ArrayListTask6 {
+
+public class EvenOddTask {
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));

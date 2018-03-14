0bcc734cdb10cc8f5befce0c9f7b934e73ef6a95
Bakam <bakamc@macOS-Sierra-10-24.local>
2018-03-13 19:40:49 -0700

    commiting words in reverse order program

--- Documents/workspace1/practice/src/com/csinc/WordsinReverseOrder.java
+++ Documents/workspace1/practice/src/com/csinc/WordsinReverseOrder.java
@@ -5,9 +5,37 @@ package com.csinc;
    Add them into a string list and display them to the screen in reverse order.
 */
 
-public class ListTask2 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
+public class WordsinReverseOrder {
     public static void main(String args)
     {
-        
+        Reader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+        //Read Strings from keyboard
+        ArrayList<String> al = new ArrayList<String>();
+        for (int i = 0; i < 5; i++)
+        {
+            try {
+                String s = br.readLine();
+                al.add(0,s);
+            }
+            catch (IOException ioe)
+            {
+                System.out.println("caught IOE");
+            }
+
+        }
+        /* Display array data to screen */
+        for (int i = 0; i < al.size(); i++)
+        {
+            int j = al.size() - i - 1;
+            System.out.println(al.get(j));
+
+        }
     }
 }

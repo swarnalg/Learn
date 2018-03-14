d99b9c720a69de1cdb9ff7ce00025f928b6405ea
Bakam <bakamc@macOS-Sierra-10-24.local>
2018-03-13 18:47:17 -0700

    wordsinReverseorder task

---
+++ Documents/workspace1/practice/src/com/csinc/WordsinReverseOrder.java
@@ -0,0 +1,13 @@
+package com.csinc;
+
+/* 5 words in reverse order
+   Read from the keyboard 5 words.
+   Add them into a string list and display them to the screen in reverse order.
+*/
+
+public class ListTask2 {
+    public static void main(String args)
+    {
+        
+    }
+}
